7926adf0f4110c8c4aeaa136c72a5e9e45036583
Vishal Shaji <vishshaji03@gmail.com>
2025-06-20 08:22:00 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
   return (
-    <div className="absolute w-full top-0 z-50 px-4 backdrop-blur-md">
+    <div className="absolute w-full top-0 z-50 px-4 backdrop-blur-xs shadow-md">
       <div className="sticky top-4 mx-auto flex justify-between items-center
                       md:w-2/3 px-6 py-4  
-                      text-white shadow-md">
+                      text-white ">
         {/* Logo */}
         <div className="font-bold text-lg tracking-wide">
-          DARJEELING LUXURY
+          <a href="/">DARJEELING LUXURY</a>
         </div>
 
         {/* Navigation Links */}
